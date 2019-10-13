059100bda5e9e14e9476b6c42e0ff920debd633d
Marvin Arnold <marvin@geeky.rocks>
2019-10-13 14:47:30 -0500

    Added hero landing

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -30,6 +30,29 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
         </footer>
+
+          {/* JS Global Compulsory */}
+          <script src="vendor/jquery/dist/jquery.min.js"></script>
+          <script src="vendor/jquery-migrate/dist/jquery-migrate.min.js"></script>
+          <script src="vendor/popper.js/dist/umd/popper.min.js"></script>
+          <script src="vendor/bootstrap/bootstrap.min.js"></script>
+
+          {/* JS Implementing Plugins */}
+          <script src="vendor/hs-megamenu/src/hs.megamenu.js"></script>
+          <script src="vendor/svg-injector/dist/svg-injector.min.js"></script>
+          <script src="vendor/appear.js"></script>
+          <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+          <script src="vendor/gmaps/gmaps.min.js"></script>
+
+          {/* JS Front */}
+          <script src="js/hs.core.js"></script>
+          <script src="js/components/hs.header.js"></script>
+          <script src="js/components/hs.unfold.js"></script>
+          <script src="js/components/hs.scroll-nav.js"></script>
+          <script src="js/components/hs.svg-injector.js"></script>
+          <script src="js/components/hs.validation.js"></script>
+          <script src="js/components/hs.g-map.js"></script>
+          <script src="js/components/hs.go-to.js"></script>
       </div>
     </>
   )
